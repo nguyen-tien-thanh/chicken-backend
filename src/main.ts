780dcf407a97285216ac024435a8b9ece35cfada
Nguyen Tien Thanh <thanhnt2@vietmap.vn>
2026-03-22 15:38:32 +0700

    feat: integrate Swagger for API documentation and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('API Docs')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get('port');
+  await app.listen(port);
+
+  Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 bootstrap();

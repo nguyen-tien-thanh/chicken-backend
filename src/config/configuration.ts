298546d8b527590f3bed4567efda224d5892c952
Nguyen Tien Thanh <thanhnt2@vietmap.vn>
2026-03-22 15:13:33 +0700

    feat: add Prisma setup and configuration, update dependencies, and modify .gitignore

---
+++ src/config/configuration.ts
@@ -0,0 +1,9 @@
+export default () => ({
+  port: parseInt(process.env.PORT ?? '3000', 10),
+  database: {
+    host: process.env.DATABASE_HOST ?? 'localhost',
+    port: parseInt(process.env.DATABASE_PORT ?? '5432', 10),
+    user: process.env.DATABASE_USER ?? 'postgres',
+    password: process.env.DATABASE_PASSWORD ?? 'secret',
+  },
+});

0e57b98c9a6a0e4cbf04e4d762dafdae46c8258d
Nguyen Tien Thanh <thanhnt2@vietmap.vn>
2026-03-22 16:05:52 +0700

    feat: add @nestjs/schedule for task scheduling and update dependencies in package.json and yarn.lock

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import configuration from './config/configuration';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, load: [configuration] })],
+  imports: [
+    ScheduleModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

1ad1734813b588fb9dd34d7ac67523ec239af1fb
Nguyen Tien Thanh <nguyentienthanh.tgdd@gmail.com>
2026-04-27 12:59:56 +0700

    refactor: remove cache-manager from nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
-import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -16,16 +14,15 @@ import { ProductModule } from './modules/product/product.module';
 import { PurchaseItemModule } from './modules/purchase-item/purchase-item.module';
 import { PurchaseModule } from './modules/purchase/purchase.module';
 import { RoleModule } from './modules/role/role.module';
+import { SaleItemModule } from './modules/sale-item/sale-item.module';
+import { SaleModule } from './modules/sale/sale.module';
 import { SupplierModule } from './modules/supplier/supplier.module';
 import { UserModule } from './modules/user/user.module';
-import { SaleModule } from './modules/sale/sale.module';
-import { SaleItemModule } from './modules/sale-item/sale-item.module';
 
 @Module({
   imports: [
     ScheduleModule.forRoot(),
     ConfigModule.forRoot({ isGlobal: true, load: [envConfig] }),
-    CacheModule.register({ isGlobal: true }),
     PrismaModule,
     AuthModule,
     RoleModule,
@@ -42,12 +39,6 @@ import { SaleItemModule } from './modules/sale-item/sale-item.module';
     SaleItemModule,
   ],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: CacheInterceptor,
-    },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
